5a5744ca7dcdada6ab66b092c0de6e69a651c193
Chris Ellis <chris@intrbiz.com>
2014-08-14 19:10:32 +0100

    Refactor to a Junit test

---
+++ src/test/java/com/intrbiz/express/sample/TestValueExpression.java
@@ -0,0 +1,34 @@
+package com.intrbiz.express.sample;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.intrbiz.express.DefaultContext;
+import com.intrbiz.express.ExpressContext;
+import com.intrbiz.express.value.ValueExpression;
+
+public class TestValueExpression
+{
+    private ExpressContext context;
+    
+    @Before
+    public void setupContext()
+    {
+        this.context = new DefaultContext();
+    }
+    
+    @Test()
+    public void testBasicValueExpression()
+    {
+        // Parse an expression
+        ValueExpression expression = new ValueExpression(context, "#{'Hello' + ' ' + 'World'}");
+        //
+        Object o = expression.get(context, null);
+        // Evaluate the expression
+        assertThat(o, is(instanceOf(String.class)));
+        assertThat((String) o, is(equalTo("Hello World")));
+    }
+}
