2f4a6d8bd63ac10ac19277b7b9d3545befcbfe0e
Chris Ellis <chris@intrbiz.com>
2014-08-15 02:03:20 +0100

    Tests for greater than or equal to

--- src/test/java/com/intrbiz/express/tests/TestEquality.java
+++ src/test/java/com/intrbiz/express/tests/TestEquality.java
@@ -515,4 +515,61 @@ public class TestEquality
         assertThat(o, is(instanceOf(Boolean.class)));
         assertThat((Boolean) o, is(equalTo(false)));
     }
+    
+    // gteq
+    
+    @Test
+    public void testGtEq1()
+    {
+        ValueExpression ve = new ValueExpression(this.context, "#{2 >= 1}");
+        Object o = ve.get(this.context, null);
+        assertThat(o, is(instanceOf(Boolean.class)));
+        assertThat((Boolean) o, is(equalTo(true)));
+    }
+    
+    @Test
+    public void testGtEq2()
+    {
+        ValueExpression ve = new ValueExpression(this.context, "#{1 >= 1}");
+        Object o = ve.get(this.context, null);
+        assertThat(o, is(instanceOf(Boolean.class)));
+        assertThat((Boolean) o, is(equalTo(true)));
+    }
+    
+    @Test
+    public void testGtEq3()
+    {
+        ValueExpression ve = new ValueExpression(this.context, "#{1 >= 2}");
+        Object o = ve.get(this.context, null);
+        assertThat(o, is(instanceOf(Boolean.class)));
+        assertThat((Boolean) o, is(equalTo(false)));
+    }
+    
+    @Test
+    public void testGtEq4()
+    {
+        ValueExpression ve = new ValueExpression(this.context, "#{2 gteq 1}");
+        Object o = ve.get(this.context, null);
+        assertThat(o, is(instanceOf(Boolean.class)));
+        assertThat((Boolean) o, is(equalTo(true)));
+    }
+    
+    @Test
+    public void testGtEq5()
+    {
+        ValueExpression ve = new ValueExpression(this.context, "#{1 gteq 1}");
+        Object o = ve.get(this.context, null);
+        assertThat(o, is(instanceOf(Boolean.class)));
+        assertThat((Boolean) o, is(equalTo(true)));
+    }
+    
+    @Test
+    public void testGtEq6()
+    {
+        ValueExpression ve = new ValueExpression(this.context, "#{1 gteq 2}");
+        Object o = ve.get(this.context, null);
+        assertThat(o, is(instanceOf(Boolean.class)));
+        assertThat((Boolean) o, is(equalTo(false)));
+    }
+    
 }
