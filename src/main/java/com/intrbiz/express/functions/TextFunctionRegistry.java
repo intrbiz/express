040f2734fe82ea0138980c2aef56180be997bf9b
Chris Ellis <chris@intrbiz.com>
2014-11-01 00:26:37 +0000

    Add join function

--- src/main/java/com/intrbiz/express/functions/TextFunctionRegistry.java
+++ src/main/java/com/intrbiz/express/functions/TextFunctionRegistry.java
@@ -1,6 +1,7 @@
 package com.intrbiz.express.functions;
 
 import com.intrbiz.express.ExpressExtensionRegistry;
+import com.intrbiz.express.functions.text.JoinFunction;
 import com.intrbiz.express.functions.text.LcFunction;
 import com.intrbiz.express.functions.text.UcFunction;
 import com.intrbiz.express.functions.text.UcfirstFunction;
@@ -14,5 +15,6 @@ public class TextFunctionRegistry extends ExpressExtensionRegistry
         this.addFunction("uc", UcFunction.class); // uppercase
         this.addFunction("lc", LcFunction.class); // lowercase
         this.addFunction("ucfirst", UcfirstFunction.class); // uppercase first character
+        this.addFunction("join", JoinFunction.class); // join a collection
     }
 }
